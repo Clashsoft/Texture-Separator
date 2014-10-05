4b8020cd4f72f955fa60e778c5928b4c4734a880
Clashsoft <clashsoft@hotmail.com>
2014-10-05 22:13:27 +0200

    Implementation

--- src/com/clashsoft/ts/TextureSeperator.java
+++ src/com/clashsoft/ts/TextureSeperator.java
@@ -1,7 +1,10 @@
 package com.clashsoft.ts;
 
 import java.awt.EventQueue;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import com.jgoodies.forms.factories.FormFactory;
@@ -11,33 +14,35 @@ import com.jgoodies.forms.layout.RowSpec;
 
 public class TextureSeperator
 {
-	private JFrame		frame;
-	public JLabel		labelTexture;
-	public JLabel		labelOffset;
-	public JLabel		labelSubtexture;
+	public static File			file;
 	
-	public JTextField	textFieldFile;
-	public JButton		buttonSelectFile;
+	private JFrame				frame;
+	public JLabel				labelTexture;
+	public JLabel				labelOffset;
+	public JLabel				labelSubtexture;
 	
-	public JLabel		labelOffsetX;
-	public JLabel		labelOffsetY;
-	public JLabel		labelWidth;
-	public JLabel		labelHeight;
+	public JTextField			textFieldFile;
+	public JButton				buttonSelectFile;
 	
-	public JSpinner		spinnerOffsetX;
-	public JSpinner		spinnerOffsetY;
-	public JSpinner		spinnerWidth;
-	public JSpinner		spinnerHeight;
+	public JLabel				labelOffsetX;
+	public JLabel				labelOffsetY;
+	public JLabel				labelWidth;
+	public JLabel				labelHeight;
 	
-	public JButton		buttonSeperate;
+	public JSpinner				spinnerOffsetX;
+	public JSpinner				spinnerOffsetY;
+	public JSpinner				spinnerWidth;
+	public JSpinner				spinnerHeight;
 	
-	/**
-	 * Launch the application.
-	 */
+	public JButton				buttonSeperate;
+	
+	private final JFileChooser	fileChooser	= new JFileChooser();
+	
 	public static void main(String[] args)
 	{
 		EventQueue.invokeLater(new Runnable()
 		{
+			@Override
 			public void run()
 			{
 				try
@@ -53,24 +58,18 @@ public class TextureSeperator
 		});
 	}
 	
-	/**
-	 * Create the application.
-	 */
 	public TextureSeperator()
 	{
 		initialize();
 	}
 	
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize()
 	{
 		this.frame = new JFrame();
 		this.frame.setTitle("Texture Seperator");
 		this.frame.setBounds(100, 100, 436, 183);
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] { FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, ColumnSpec.decode("max(43dlu;default):grow"), FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, ColumnSpec.decode("max(40dlu;default):grow"), FormFactory.RELATED_GAP_COLSPEC, ColumnSpec.decode("max(31dlu;default)"), FormFactory.RELATED_GAP_COLSPEC, }, new RowSpec[] { FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, }));
+		this.frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] { FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, FormFactory.MIN_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, FormFactory.GLUE_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, ColumnSpec.decode("max(46dlu;default)"), FormFactory.RELATED_GAP_COLSPEC, ColumnSpec.decode("max(31dlu;default)"), FormFactory.RELATED_GAP_COLSPEC, }, new RowSpec[] { FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, }));
 		
 		this.labelTexture = new JLabel("Texture:");
 		this.frame.getContentPane().add(this.labelTexture, "2, 2, right, default");
@@ -86,6 +85,13 @@ public class TextureSeperator
 		this.frame.getContentPane().add(this.textFieldFile, "4, 2, 7, 1, fill, default");
 		
 		this.buttonSelectFile = new JButton("...");
+		this.buttonSelectFile.addActionListener(e -> {
+			int ret = fileChooser.showOpenDialog(frame);
+			if (ret == JFileChooser.APPROVE_OPTION)
+			{
+				file = fileChooser.getSelectedFile();
+			}
+		});
 		this.frame.getContentPane().add(this.buttonSelectFile, "12, 2");
 		
 		this.labelOffsetX = new JLabel("x:");
@@ -100,20 +106,78 @@ public class TextureSeperator
 		this.labelHeight = new JLabel("h:");
 		this.frame.getContentPane().add(this.labelHeight, "8, 6");
 		
-		this.spinnerOffsetX = new JSpinner();
+		this.spinnerOffsetX = new JSpinner(new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1));
 		this.frame.getContentPane().add(this.spinnerOffsetX, "6, 4");
 		
-		this.spinnerOffsetY = new JSpinner();
+		this.spinnerOffsetY = new JSpinner(new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1));
 		this.frame.getContentPane().add(this.spinnerOffsetY, "10, 4, 3, 1");
 		
-		this.spinnerWidth = new JSpinner();
+		this.spinnerWidth = new JSpinner(new SpinnerNumberModel(16, 0, Integer.MAX_VALUE, 1));
 		this.frame.getContentPane().add(this.spinnerWidth, "6, 6");
 		
-		this.spinnerHeight = new JSpinner();
+		this.spinnerHeight = new JSpinner(new SpinnerNumberModel(16, 0, Integer.MAX_VALUE, 1));
 		this.frame.getContentPane().add(this.spinnerHeight, "10, 6, 3, 1");
 		
 		this.buttonSeperate = new JButton("Seperate");
+		this.buttonSeperate.addActionListener(e -> {
+			
+			int offsetX = (int) spinnerOffsetX.getValue();
+			int offsetY = (int) spinnerOffsetY.getValue();
+			int width = (int) spinnerWidth.getValue();
+			int height = (int) spinnerHeight.getValue();
+			seperate(file, offsetX, offsetY, width, height);
+		});
 		this.frame.getContentPane().add(this.buttonSeperate, "2, 8, 11, 3");
 	}
 	
+	public void seperate(File file, int offsetX, int offsetY, int width, int height)
+	{
+		try
+		{
+			String path = file.getAbsolutePath();
+			int index = path.lastIndexOf('.');
+			if (index == -1)
+			{
+				throw new IllegalArgumentException("Invalid File");
+			}
+			
+			File parent = new File(path.substring(0, index));
+			parent.mkdirs();
+			
+			BufferedImage image = ImageIO.read(file);
+			
+			int imageWidth = image.getWidth();
+			int imageHeight = image.getHeight();
+			int offsetWidth = (imageWidth - offsetX);
+			int offsetHeight = (imageHeight - offsetY);
+			int countX = offsetWidth / width;
+			int countY = offsetHeight / height;
+			
+			int count = 0;
+			
+			for (int x = 0; x < countX; x++)
+			{
+				for (int y = 0; y < countY; y++)
+				{
+					try
+					{
+						BufferedImage subimage = image.getSubimage(offsetX + x * width, offsetY + y * height, width, height);
+						File newFile = new File(parent, "texture_" + x + "_" + y + ".png");
+						ImageIO.write(subimage, "png", newFile);
+						count++;
+					}
+					catch (Exception ex)
+					{
+						ex.printStackTrace();
+					}
+				}
+			}
+			
+			JOptionPane.showMessageDialog(this.frame, String.format("Successfully generated %d / %d sub-textures.", count, countX * countY));
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
 }
