49a0c1c34cd3fd96c0f18a3088e7fd240cc44338
Clashsoft <clashsoft@hotmail.com>
2014-10-05 21:36:12 +0200

    Added GUI

--- src/com/clashsoft/ts/TextureSeperator.java
+++ src/com/clashsoft/ts/TextureSeperator.java
@@ -2,19 +2,34 @@ package com.clashsoft.ts;
 
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import java.awt.GridLayout;
-import javax.swing.JLabel;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import com.jgoodies.forms.layout.FormLayout;
+import javax.swing.*;
+
+import com.jgoodies.forms.factories.FormFactory;
 import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.RowSpec;
 
 public class TextureSeperator
 {
+	private JFrame		frame;
+	public JLabel		labelTexture;
+	public JLabel		labelOffset;
+	public JLabel		labelSubtexture;
 	
-	private JFrame	frmTextureSeperator;
+	public JTextField	textFieldFile;
+	public JButton		buttonSelectFile;
+	
+	public JLabel		labelOffsetX;
+	public JLabel		labelOffsetY;
+	public JLabel		labelWidth;
+	public JLabel		labelHeight;
+	
+	public JSpinner		spinnerOffsetX;
+	public JSpinner		spinnerOffsetY;
+	public JSpinner		spinnerWidth;
+	public JSpinner		spinnerHeight;
+	
+	public JButton		buttonSeperate;
 	
 	/**
 	 * Launch the application.
@@ -28,7 +43,7 @@ public class TextureSeperator
 				try
 				{
 					TextureSeperator window = new TextureSeperator();
-					window.frmTextureSeperator.setVisible(true);
+					window.frame.setVisible(true);
 				}
 				catch (Exception e)
 				{
@@ -51,10 +66,54 @@ public class TextureSeperator
 	 */
 	private void initialize()
 	{
-		this.frmTextureSeperator = new JFrame();
-		this.frmTextureSeperator.setTitle("Texture Seperator");
-		this.frmTextureSeperator.setBounds(100, 100, 450, 300);
-		this.frmTextureSeperator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame = new JFrame();
+		this.frame.setTitle("Texture Seperator");
+		this.frame.setBounds(100, 100, 436, 183);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] { FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, ColumnSpec.decode("max(43dlu;default):grow"), FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, ColumnSpec.decode("max(40dlu;default):grow"), FormFactory.RELATED_GAP_COLSPEC, ColumnSpec.decode("max(31dlu;default)"), FormFactory.RELATED_GAP_COLSPEC, }, new RowSpec[] { FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, }));
+		
+		this.labelTexture = new JLabel("Texture:");
+		this.frame.getContentPane().add(this.labelTexture, "2, 2, right, default");
+		
+		this.labelOffset = new JLabel("Offset:");
+		this.frame.getContentPane().add(this.labelOffset, "2, 4, right, default");
+		
+		this.labelSubtexture = new JLabel("Sub-Texture:");
+		this.frame.getContentPane().add(this.labelSubtexture, "2, 6, right, default");
+		
+		this.textFieldFile = new JTextField();
+		this.textFieldFile.setColumns(10);
+		this.frame.getContentPane().add(this.textFieldFile, "4, 2, 7, 1, fill, default");
+		
+		this.buttonSelectFile = new JButton("...");
+		this.frame.getContentPane().add(this.buttonSelectFile, "12, 2");
+		
+		this.labelOffsetX = new JLabel("x:");
+		this.frame.getContentPane().add(this.labelOffsetX, "4, 4");
+		
+		this.labelOffsetY = new JLabel("y:");
+		this.frame.getContentPane().add(this.labelOffsetY, "8, 4");
+		
+		this.labelWidth = new JLabel("w:");
+		this.frame.getContentPane().add(this.labelWidth, "4, 6");
+		
+		this.labelHeight = new JLabel("h:");
+		this.frame.getContentPane().add(this.labelHeight, "8, 6");
+		
+		this.spinnerOffsetX = new JSpinner();
+		this.frame.getContentPane().add(this.spinnerOffsetX, "6, 4");
+		
+		this.spinnerOffsetY = new JSpinner();
+		this.frame.getContentPane().add(this.spinnerOffsetY, "10, 4, 3, 1");
+		
+		this.spinnerWidth = new JSpinner();
+		this.frame.getContentPane().add(this.spinnerWidth, "6, 6");
+		
+		this.spinnerHeight = new JSpinner();
+		this.frame.getContentPane().add(this.spinnerHeight, "10, 6, 3, 1");
+		
+		this.buttonSeperate = new JButton("Seperate");
+		this.frame.getContentPane().add(this.buttonSeperate, "2, 8, 11, 3");
 	}
 	
 }
