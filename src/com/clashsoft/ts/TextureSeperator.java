d8dad8b40d609fb049b5782144ae88e4ec9eb0e2
Clashsoft <clashsoft@hotmail.com>
2014-10-05 23:34:01 +0200

    Added Cancel Button, improved GUI

--- src/com/clashsoft/ts/TextureSeperator.java
+++ src/com/clashsoft/ts/TextureSeperator.java
@@ -33,8 +33,12 @@ public class TextureSeperator
 	
 	public JButton				buttonSeperate;
 	
-	private final JFileChooser	fileChooser	= new JFileChooser();
+	public JButton				buttonCancel;
+	
 	public JProgressBar			progressBar;
+	private final JFileChooser	fileChooser	= new JFileChooser();
+	
+	public boolean				cancel;
 	
 	public static void main(String[] args)
 	{
@@ -51,24 +55,35 @@ public class TextureSeperator
 	{
 		this.frame = new JFrame();
 		this.frame.setTitle("Texture Seperator");
-		this.frame.setBounds(100, 100, 436, 222);
+		this.frame.setBounds(100, 100, 420, 160);
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] { FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, FormFactory.MIN_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, FormFactory.GLUE_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, ColumnSpec.decode("max(46dlu;default)"), FormFactory.RELATED_GAP_COLSPEC, ColumnSpec.decode("max(31dlu;default)"), FormFactory.RELATED_GAP_COLSPEC, }, new RowSpec[] { FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, RowSpec.decode("default:grow"), FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, }));
+		this.frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] { FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, ColumnSpec.decode("50dlu:grow"), FormFactory.RELATED_GAP_COLSPEC, FormFactory.DEFAULT_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, ColumnSpec.decode("50dlu:grow"), FormFactory.RELATED_GAP_COLSPEC, FormFactory.PREF_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, }, new RowSpec[] { FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, FormFactory.DEFAULT_ROWSPEC, FormFactory.RELATED_GAP_ROWSPEC, }));
 		
 		this.labelTexture = new JLabel("Texture:");
-		this.frame.getContentPane().add(this.labelTexture, "2, 2, right, default");
+		this.frame.getContentPane().add(this.labelTexture, "2, 2, 3, 1, left, default");
 		
 		this.labelOffset = new JLabel("Offset:");
-		this.frame.getContentPane().add(this.labelOffset, "2, 4, right, default");
+		this.frame.getContentPane().add(this.labelOffset, "2, 4, left, default");
 		
-		this.labelSubtexture = new JLabel("Sub-Texture:");
-		this.frame.getContentPane().add(this.labelSubtexture, "2, 6, right, default");
+		this.buttonSeperate = new JButton("Seperate");
+		this.buttonSeperate.addActionListener(e -> {
+			File file = new File(this.textFieldFile.getText());
+			int offsetX = (int) this.spinnerOffsetX.getValue();
+			int offsetY = (int) this.spinnerOffsetY.getValue();
+			int width = (int) this.spinnerWidth.getValue();
+			int height = (int) this.spinnerHeight.getValue();
+			this.seperate(file, offsetX, offsetY, width, height);
+		});
+		this.frame.getContentPane().add(this.buttonSeperate, "12, 4, 1, 3, fill, fill");
+		
+		this.labelSubtexture = new JLabel("Size:");
+		this.frame.getContentPane().add(this.labelSubtexture, "2, 6, left, default");
 		
 		this.textFieldFile = new JTextField();
 		this.textFieldFile.setColumns(10);
-		this.frame.getContentPane().add(this.textFieldFile, "4, 2, 7, 1, fill, default");
+		this.frame.getContentPane().add(this.textFieldFile, "6, 2, 5, 1, fill, default");
 		
-		this.buttonSelectFile = new JButton("...");
+		this.buttonSelectFile = new JButton("Select");
 		this.buttonSelectFile.addActionListener(e -> {
 			int ret = this.fileChooser.showOpenDialog(this.frame);
 			if (ret == JFileChooser.APPROVE_OPTION)
@@ -95,29 +110,24 @@ public class TextureSeperator
 		this.frame.getContentPane().add(this.spinnerOffsetX, "6, 4");
 		
 		this.spinnerOffsetY = new JSpinner(new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1));
-		this.frame.getContentPane().add(this.spinnerOffsetY, "10, 4, 3, 1");
+		this.frame.getContentPane().add(this.spinnerOffsetY, "10, 4");
 		
 		this.spinnerWidth = new JSpinner(new SpinnerNumberModel(16, 0, Integer.MAX_VALUE, 1));
 		this.frame.getContentPane().add(this.spinnerWidth, "6, 6");
 		
 		this.spinnerHeight = new JSpinner(new SpinnerNumberModel(16, 0, Integer.MAX_VALUE, 1));
-		this.frame.getContentPane().add(this.spinnerHeight, "10, 6, 3, 1");
-		
-		this.buttonSeperate = new JButton("Seperate");
-		this.buttonSeperate.addActionListener(e -> {
-			File file = new File(this.textFieldFile.getText());
-			int offsetX = (int) this.spinnerOffsetX.getValue();
-			int offsetY = (int) this.spinnerOffsetY.getValue();
-			int width = (int) this.spinnerWidth.getValue();
-			int height = (int) this.spinnerHeight.getValue();
-			this.seperate(file, offsetX, offsetY, width, height);
-		});
-		this.frame.getContentPane().add(this.buttonSeperate, "2, 8, 11, 1, fill, fill");
+		this.frame.getContentPane().add(this.spinnerHeight, "10, 6");
 		
 		this.progressBar = new JProgressBar();
 		this.progressBar.setStringPainted(true);
-		this.progressBar.setString("");
-		this.frame.getContentPane().add(this.progressBar, "2, 10, 11, 1");
+		this.progressBar.setString("0 / 0");
+		this.frame.getContentPane().add(this.progressBar, "2, 8, 9, 1");
+		
+		this.buttonCancel = new JButton("Cancel");
+		this.buttonCancel.addActionListener(e -> {
+			this.cancel = true;
+		});
+		this.frame.getContentPane().add(this.buttonCancel, "12, 8");
 	}
 	
 	public void seperate(File file, int offsetX, int offsetY, int width, int height)
@@ -125,7 +135,7 @@ public class TextureSeperator
 		new Thread(() -> {
 			try
 			{
-				seperate_(file, offsetX, offsetY, width, height);
+				this.seperate_(file, offsetX, offsetY, width, height);
 			}
 			catch (Exception ex)
 			{
@@ -149,7 +159,7 @@ public class TextureSeperator
 		int count = 0;
 		int totalCount = countX * countY;
 		
-		if (totalCount > 256)
+		if (totalCount > 4096)
 		{
 			int i = JOptionPane.showConfirmDialog(this.frame, "Are you sure you want to generate " + totalCount + " Sub-Texture files?", "Confirm large Operation", JOptionPane.YES_NO_OPTION);
 			if (i != JOptionPane.YES_OPTION)
@@ -175,6 +185,11 @@ public class TextureSeperator
 		{
 			for (int y = 0; y < countY; y++)
 			{
+				if (this.cancel)
+				{
+					break;
+				}
+				
 				try
 				{
 					BufferedImage subimage = image.getSubimage(offsetX + x * width, offsetY + y * height, width, height);
@@ -186,8 +201,7 @@ public class TextureSeperator
 					this.progressBar.setString(count + " / " + totalCount);
 				}
 				catch (Exception ex)
-				{
-				}
+				{}
 			}
 		}
 		
@@ -196,5 +210,7 @@ public class TextureSeperator
 		
 		String message = String.format("Successfully generated %d / %d Sub-Textures in %d s (%d ms, %d ms per Sub-Texture).", count, totalCount, time / 1000L, time, timePerImage);
 		JOptionPane.showMessageDialog(this.frame, message, "Texture Seperator", JOptionPane.INFORMATION_MESSAGE);
+		
+		this.cancel = false;
 	}
 }
