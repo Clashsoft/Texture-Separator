4608d98a0e4791770b236f8f9549cae8d9135edb
Clashsoft <clashsoft@hotmail.com>
2014-10-06 13:07:05 +0200

    1.0.0

--- src/com/clashsoft/ts/TextureSeperator.java
+++ src/com/clashsoft/ts/TextureSeperator.java
@@ -14,6 +14,8 @@ import com.jgoodies.forms.layout.RowSpec;
 
 public class TextureSeperator
 {
+	public static final String	VERSION		= "1.0.0";
+	
 	private JFrame				frame;
 	public JLabel				labelTexture;
 	public JLabel				labelOffset;
